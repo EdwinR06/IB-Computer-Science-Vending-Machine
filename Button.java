f247b635f261fdc6bed924962d620f8809ec2ccd
Edwin Ryerson <eryerson06@gmail.com>
2022-11-15 11:28:27 -0700

    Frame Button Class

--- Button.java
+++ Button.java
@@ -3,4 +3,16 @@ public class Button {
     private int position;
     public boolean pressed;
     private String itemName;
+
+    public Button() {
+
+    }
+
+    public void setCost(double cost) {}
+
+   // public double getCost() {}
+
+   public void setItemName(String name) {}
+
+   // public String getItemName() {}
 }
