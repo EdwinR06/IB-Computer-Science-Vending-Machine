a08e2c6337c581dd68d873d9367de14eac173666
Edwin Ryerson <eryerson06@gmail.com>
2022-11-15 11:37:56 -0700

    Frame Vending Machine Methods

--- VendingMachine.java
+++ VendingMachine.java
@@ -4,5 +4,15 @@ public class VendingMachine {
     private int size;
     private boolean paid;
 
-    
+    public VendingMachine(int size) {}
+
+    public void setCost(int button, double cost) {}
+
+    public void refill(int coil) {}
+
+    public void makePayment(double amount) {}
+
+    public void pressButton(int button) {}
+
+    // private double calculateChange(double amountInput, double cost) {}
 }
