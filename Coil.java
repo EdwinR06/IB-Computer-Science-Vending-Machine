afb8fa222077f800d55978f3631998cfbda9724a
Edwin Ryerson <eryerson06@gmail.com>
2022-11-15 11:30:40 -0700

    Frame Coil Methods

--- Coil.java
+++ Coil.java
@@ -2,4 +2,16 @@ public class Coil {
     private int numSlots;
     private int position;
     private int itemsRemaining;
+
+    public Coil() {
+
+    }
+
+    public void spin() {}
+
+    // public int getItemsRemaining() {}
+
+    public void refill() {}
+
+    private void sendRestockAlert() {}
 }
