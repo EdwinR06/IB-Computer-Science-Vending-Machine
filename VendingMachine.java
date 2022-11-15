c1d4f455d8eee965b8b7250b964ada05629197ff
Edwin Ryerson <eryerson06@gmail.com>
2022-11-15 12:01:08 -0700

    Create Make Buttons & Make Coils Methods

--- VendingMachine.java
+++ VendingMachine.java
@@ -3,13 +3,30 @@ public class VendingMachine {
     private Button[] buttons;
     private int size;
     private double payment;
+    private int slots;
 
-
-    public VendingMachine(int size) {
+    public VendingMachine(int size, int slots) {
         this.coils=new Coil[size];
         this.buttons=new Button[size];
         this.size=size;
         this.payment=0;
+        this.slots=slots;
+
+        makeButtons();
+        makeCoils();
+
+    }
+
+    private void makeButtons(){
+        for(int i=0; i<this.size; i++){
+            this.buttons[i]=new Button(i);
+        }
+    }
+
+    private void makeCoils(){
+        for(int i=0; i<this.size; i++){
+            this.coils[i]=new Coil(i, this.slots);
+        }
     }
 
     public void setCost(int button, double cost) {}
